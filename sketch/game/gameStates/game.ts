192b24eb75050622adfdd01e48bcf614fc4dfe54
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2023-05-26 18:26:01 -0500

    quickfixes

--- sketch/game/gameStates/game.ts
+++ sketch/game/gameStates/game.ts
@@ -1,6 +1,6 @@
 function gamePlaying(manager: GameManager) {
   manager.addState(GameStates.GAME_PLAYING, (m) => {
-    background(0);
+    background(100, 50, 50);
     manager.runEntities();
   });
 }
