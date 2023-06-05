fd5fc5b81ad82cd0c77a70a88180e2f7765ee86c
Rodrigo Morais <rmoraisu@protonmail.com>
2023-06-05 08:49:34 -0500

    revert joystick to floating

--- sketch/game/controls/joystick.ts
+++ sketch/game/controls/joystick.ts
@@ -24,7 +24,7 @@ class Joystick extends EntityFactory {
       "joystick",
       0,
       { width: manager.UnitSize * 2, height: manager.UnitSize * 3 },
-      { x: width * 0.18, y: height * 0.25 }
+      { x: 0, y: 0 }
     );
 
     Joystick.controlEvent(manager, joystick);
@@ -34,32 +34,30 @@ class Joystick extends EntityFactory {
   }
 
   static draw(manager: GameManager, joystick: Entity) {
-    // const joystickSize = manager.UnitSize * 1.5;
-    // joystick.addListener(
-    //   Joystick.Events.ControlEvent.name,
-    //   (options: ControllerOptions) => {
-    //     let { currentPress, isPressed } = options;
-    //     if (isPressed) {
-    //       fill(255, 90);
-    //       circle(0, 0, joystickSize);
-    //       stroke(255, 0, 0);
-    //       strokeWeight(3);
-    //       const norm = currentPress.copy();
-    //       if (norm.x ** 2 + norm.y ** 2 > (joystickSize * 0.4) ** 2)
-    //         norm.normalize().mult(joystickSize * 0.4);
-    //       line(0, 0, norm.x, norm.y);
-    //       fill(255);
-    //       strokeWeight(1);
-    //       stroke(0);
-    //       circle(norm.x, norm.y, joystickSize * 0.7);
-    //     }
-    //   }
-    // );
+    const joystickSize = manager.UnitSize * 1.5;
+    joystick.addListener(
+      Joystick.Events.ControlEvent.name,
+      (options: ControllerOptions) => {
+        let { currentPress, isPressed } = options;
+        if (isPressed) {
+          fill(255, 90);
+          circle(0, 0, joystickSize);
+          stroke(255, 0, 0);
+          strokeWeight(3);
+          const norm = currentPress.copy();
+          if (norm.x ** 2 + norm.y ** 2 > (joystickSize * 0.4) ** 2)
+            norm.normalize().mult(joystickSize * 0.4);
+          line(0, 0, norm.x, norm.y);
+          fill(255);
+          strokeWeight(1);
+          stroke(0);
+          circle(norm.x, norm.y, joystickSize * 0.7);
+        }
+      }
+    );
   }
 
   static controlEvent(manager: GameManager, joystick: Entity) {
-    const joystickSize = manager.UnitSize * 2;
-
     joystick.addBehavior(
       Joystick.Behaviors.EmitControlEvent,
       (e) => {
@@ -68,41 +66,24 @@ class Joystick extends EntityFactory {
 
         if (options === undefined) {
           options = {
-            origin: joystick.position.copy(),
+            origin: createVector(0, 0),
             isPressed: false,
             currentPress: createVector(0, 0),
           };
         }
         const [x, y] = [mouseX, mouseY];
-        // if (!options.isPressed) options.origin = createVector(0, 0);
+        if (!options.isPressed)
+          options.origin = createVector(x - width / 2, y - height / 2);
         options.isPressed = false;
 
         if (mouseIsPressed) {
           options.isPressed = true;
           options.currentPress = createVector(x - width / 2, y - height / 2);
           options.currentPress.sub(options.origin);
-          // joystick.setPosition(options.origin);
+          joystick.setPosition(options.origin);
         }
 
         manager.addEvent(Joystick.Events.ControlEvent.name, options, true);
-
-        if (!mouseIsPressed) {
-          options.currentPress.x = 0;
-          options.currentPress.y = 0;
-        }
-
-        fill(255, 90);
-        circle(0, 0, joystickSize);
-        stroke(255, 0, 0);
-        strokeWeight(3);
-        const norm = options.currentPress.copy();
-        if (norm.x ** 2 + norm.y ** 2 > (joystickSize * 0.4) ** 2)
-          norm.normalize().mult(joystickSize * 0.4);
-        // line(0, 0, norm.x, norm.y);
-        fill(255, 150);
-        strokeWeight(1);
-        stroke(0);
-        circle(norm.x, norm.y, joystickSize * 0.7);
       },
       true
     );
