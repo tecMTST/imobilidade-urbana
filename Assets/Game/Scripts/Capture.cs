cbbcd01ce41c60527c7a997625b675934b8a4b53
heigon <h217638@dac.unicamp.br>
2023-08-25 18:23:17 -0300

    Capture and game over

--- Assets/Game/Scripts/Capture.cs
+++ Assets/Game/Scripts/Capture.cs
@@ -4,9 +4,10 @@ using UnityEngine.SceneManagement;
 public class GameOverBothObjectsCheck : MonoBehaviour
 {
     public string gameOverSceneName = "GameOverScene"; 
-    public Transform objectToCheck1; // Transform of the first object to check
-    public Transform objectToCheck2; // Transform of the second object to check
-    // public float detectionRadius = 2.0f; // Radius of the detection area
+    public Transform objectToCheck1;
+    public Transform objectToCheck2;
+    public float detectionRadius1 = 2.0f;
+    public float detectionRadius2 = 2.0f;
 
     private void Update()
     {
@@ -14,13 +15,8 @@ public class GameOverBothObjectsCheck : MonoBehaviour
         Vector2 object1Position = objectToCheck1.position;
         Vector2 object2Position = objectToCheck2.position;
 
-        // Check if both objects are within the detection radius
-        bool isObject1InArea = Vector2.Distance(squareObjectPosition, object1Position) < 7f;
-        bool isObject2InArea = Vector2.Distance(squareObjectPosition, object2Position) < 9.3f;
-
-        // Print distances for debugging
-        Debug.Log("Distance to Object 1: " + Vector2.Distance(squareObjectPosition, object1Position));
-        Debug.Log("Distance to Object 2: " + Vector2.Distance(squareObjectPosition, object2Position));
+        bool isObject1InArea = Vector2.Distance(squareObjectPosition, object1Position) < detectionRadius1;
+        bool isObject2InArea = Vector2.Distance(squareObjectPosition, object2Position) < detectionRadius2;
 
         if (isObject1InArea && isObject2InArea)
         {
