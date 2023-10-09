71f2a7414747c4645a773593950202396b8a3661
DanielSanLopes <DanielSan_Lopes@outlook.com>
2023-10-09 02:10:57 -0300

    Danger Areas (adding and refactoring)

--- Assets/Game/Scripts/Capture.cs
+++ Assets/Game/Scripts/Capture.cs
@@ -1,26 +1,39 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameOverBothObjectsCheck : MonoBehaviour
+public class FOverBothObjectsCheck : MonoBehaviour
 {
     public string gameOverSceneName = "GameOverScene"; 
-    public Transform objectToCheck1;
-    public Transform objectToCheck2;
-    public float detectionRadius1 = 2.0f;
-    public float detectionRadius2 = 2.0f;
+ 
+    private Collider2D thisCollider;
 
-    private void Update()
-    {
-        Vector2 squareObjectPosition = transform.position;
-        Vector2 object1Position = objectToCheck1.position;
-        Vector2 object2Position = objectToCheck2.position;
 
-        bool isObject1InArea = Vector2.Distance(squareObjectPosition, object1Position) < detectionRadius1;
-        bool isObject2InArea = Vector2.Distance(squareObjectPosition, object2Position) < detectionRadius2;
-
-        if (isObject1InArea && isObject2InArea)
-        {
-            SceneManager.LoadScene(gameOverSceneName);
-        }
+    private void Start() {
+        thisCollider = this.GetComponent<Collider2D>();
     }
+
+    private void Update(){
+                
+        
+        List<Collider2D> colliders = new ();
+        ContactFilter2D contactFilter = new ContactFilter2D();
+        contactFilter.NoFilter();
+        
+        thisCollider.OverlapCollider(contactFilter, colliders);
+
+        if (colliders.Exists(item => item.CompareTag("Player"))    &&    colliders.Exists(item => item.CompareTag("Enemy")))
+        SceneManager.LoadScene(gameOverSceneName);
+            
+        
+
+    }
+
 }
+
+
+
+
+
+
