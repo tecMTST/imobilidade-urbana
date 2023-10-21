66ec9f521d13d6f88edb604925b0ad46c884c527
Gabriel Brito <dosgabrielbrito@gmail.com>
2023-10-21 15:26:25 -0300

    SFX Monstro

--- Assets/Game/Scripts/Monster1.cs
+++ Assets/Game/Scripts/Monster1.cs
@@ -10,10 +10,23 @@ public class Monster1 : MonoBehaviour
     private float distanceMoved = 0f;
     private bool shouldMove = false;
 
+    //Variáveis Áudio:
+    [SerializeField] private float limitVoice = 8f;
+    private float timerVoice = 0f;
+    private int typeVoice = 1;
+
+    //Compopnentes:
+    private AudioSource audioSource;
+
+    //Lista de SFX:
+    [SerializeField] private AudioClip sfxVoice1;
+    [SerializeField] private AudioClip sfxVoice2;
+
     void Start()
     {
         initialPosition = transform.position;
         Invoke("StartMoving", delayAfterTeleport);
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -30,6 +43,26 @@ public class Monster1 : MonoBehaviour
                 distanceMoved = 0f;
                 shouldMove = false;
                 Invoke("StartMoving", delayAfterTeleport);
+            }
+        }
+
+        //SFX Grunindos:
+        timerVoice = timerVoice + Time.deltaTime;
+        if (timerVoice > limitVoice)
+        {
+            switch (typeVoice)
+            {
+                case 1:
+                    audioSource.PlayOneShot(sfxVoice1);
+                    typeVoice = 2;
+                    timerVoice = 0f;
+                    break;
+
+                case 2:
+                    audioSource.PlayOneShot(sfxVoice2);
+                    typeVoice = 1;
+                    timerVoice = 0f;
+                    break;
             }
         }
     }
