4069cbd4707ddabdabc8a9ddb794f1a055b8c49b
DanielSanLopes <DanielSan_Lopes@outlook.com>
2023-11-02 18:46:06 -0300

    lightModes

--- Assets/Game/Scripts/DevMode/DeveloperMode.cs
+++ Assets/Game/Scripts/DevMode/DeveloperMode.cs
@@ -1,15 +1,19 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 public class DeveloperMode : MonoBehaviour{
 
 
     public Light2D GlobalLightCity;
+    public Light2D[] lamps;
+    public Volume volume;
+    public GameObject cityBack;
 
-    private bool isDeveloperMode = false;
 
     // Start is called before the first frame update
     void Start()
@@ -25,8 +29,6 @@ public class DeveloperMode : MonoBehaviour{
 
     public void SetDeveloperMode(bool devMode) {
 
-        isDeveloperMode = devMode;
-
         switch (devMode) { 
             case true:
                 DevMode();
@@ -40,17 +42,51 @@ public class DeveloperMode : MonoBehaviour{
 
     }
 
+    public void SetLightMode(bool lampMode) {
+        switch (lampMode) {
+            case true:
+                LampsLightining();
+                break;
+
+            case false:
+                CityLightining();
+                break;
+
+        }
+    }
+
     private void DevMode() {
 
     }
 
     private void StandardMode() {
+        
 
     }
 
-    private void LightiningOne() { 
+    private void LampsLightining() {
+        SetPostProcessing(false);
+        GlobalLightCity.enabled = false;
+
+        foreach (Light2D lamp in lamps) 
+            lamp.enabled = true;
+        
+        cityBack.SetActive(false);  
     }
 
-    private void LightiningTwo() { 
+    private void CityLightining() {
+        SetPostProcessing(true);
+        GlobalLightCity.enabled = true;
+
+        foreach (Light2D lamp in lamps) 
+            lamp.enabled = false;
+        
+        cityBack.SetActive(true);
+
+
+    }
+
+    private void SetPostProcessing(bool on) {
+        volume.enabled = on;   
     }
 }
