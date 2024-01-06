131546413d5ae524564b857ffe81fb17004c32bd
brunoastephan <brunoastephan@gmail.com>
2024-01-05 22:23:25 -0300

    Arrumando cena de introdução

---
+++ Assets/ShakeTrainIntrod.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeTrainIntrod : MonoBehaviour
+{
+
+    [SerializeField] float speed = 20.0f; //how fast it shakes
+    [SerializeField] float amount = 0.01f; //how much it shakes
+    
+    void Update()
+    {
+        transform.position = new Vector3(this.transform.position.x, (Mathf.Sin(Time.time * speed) * amount) - 3.5f, this.transform.position.z);
+    }
+ 
+}
